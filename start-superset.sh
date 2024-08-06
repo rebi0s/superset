b4bf21e78c73ea67e210046ec485f61505d060d8
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2024-08-05 22:16:47 -0300

    Add script for img

--- start-superset.sh
+++ start-superset.sh
@@ -1,6 +1,3 @@
 docker compose -f ../superset/docker-compose-non-dev.yml down | true
 docker compose -f ../superset/docker-compose-non-dev.yml up -d $1
 
-#docker compose -f ../superset/docker-compose-image-tag.yml down | true
-#docker compose -f ../superset/docker-compose-image-tag.yml up -d $1
-
--- stop-superset.sh
+++ stop-superset.sh
@@ -1,3 +1,4 @@
 docker compose -f ../superset/docker-compose-image-tag.yml down | true
 docker compose -f ../superset/docker-compose-non-dev.yml down | true
+docker compose -f ../superset/docker-compose.yml down | true
 
