ed84c88b1f7199cf9445ae3641daee72f4ff31d0
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2024-08-06 21:33:45 -0300

    Test custom color frontend.

--- superset-frontend/src/explore/components/controls/ConditionalFormattingControl/FormattingPopoverContent.tsx
+++ superset-frontend/src/explore/components/controls/ConditionalFormattingControl/FormattingPopoverContent.tsx
@@ -46,6 +46,7 @@ const colorSchemeOptions = (theme: SupersetTheme) => [
   { value: theme.colors.success.dark1, label: t('success dark') },
   { value: theme.colors.alert.dark1, label: t('alert dark') },
   { value: theme.colors.error.dark1, label: t('error dark') },
+  { value: '#FF0000', label: t('Lacuna 1') },
 ];
 
 const operatorOptions = [
